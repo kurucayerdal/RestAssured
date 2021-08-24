5fd2180041a16f3bf2cd4c5f449fdb028bf09ac4
kurucayerdal <kurucayerdal@gmail.com>
2021-08-24 20:21:03 +0200

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,26 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+
+    @Test
+    public void test(){
+
+        given()
+                //hazırlık işlemlerini yapacağız
+
+
+                .when()
+                //link ve aksiyon işlemleri
+
+
+                //test ve extract işlemleri
+                .then()
+
+
+        ;
+    }
+}
