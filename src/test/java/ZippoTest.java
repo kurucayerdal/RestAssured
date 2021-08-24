a9bb587ea09a0fd250f19450361b7934d2f3a61a
kurucayerdal <kurucayerdal@gmail.com>
2021-08-24 22:04:15 +0200

    state check is done

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,4 +1,5 @@
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -36,9 +37,64 @@ public class ZippoTest {
                 .then()
                 .log().body() //sadece body'i yazıyor
                 .log().all() //tüm response'u yazıyor
-        ;
+                .statusCode(200) //status kontrolü - hata olmadığında bir şey yazmıyor!
+
+               ;
     }
 
+    @Test
+    public void contentTypeTest(){
+
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .contentType(ContentType.JSON)
+                ;
+    }
+
+
+    @Test
+    public void logTest(){
+
+        given()
+                .log().all()  //request kısmı
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                ;
+    }
+
+    @Test
+    public void checkStateInResponseBody(){
+
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .body("country", equalTo("United States")) //Dışarıda ayrı bir assert yazmadan sonucu kontrol etmeye
+                                                                    //yarayan metod hamcrest yöntemi.
+                .statusCode(200)
+                ;
+
+    }
+
+    @Test
+    public void checkStateInResponseBodyArray(){
+
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .body("places[0].state", equalTo("California"))
+                .statusCode(200)
+        ;
+
+    }
 
 
 
